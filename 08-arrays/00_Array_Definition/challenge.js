4edd49b659da88f62365ed9f1d0ec4f49ecfb3bc
Irwin Litvak <irwin.litvak@gmail.com>
2014-10-09 19:55:47 +0000

    difficult, but complete

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,22 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(w, x, y, z) {
+  
+  var myArray = [w,x,y,z]
+  return myArray;
+  
+                 
+}
 
-module.exports.firstAndLast = undefined;
+
+module.exports.firstAndLast = function(w) {
+             
+  var first = w.shift();
+  var last = w.pop();
+  var array = [first, last];
+  return array;
+              
+  
+}
+       
+            
+
+
