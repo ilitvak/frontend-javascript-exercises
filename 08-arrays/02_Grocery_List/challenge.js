1222137346467d710c58f2bee64b530bae5aef5d
Irwin Litvak <irwin.litvak@gmail.com>
2014-10-09 21:42:29 +0000

    Array_Functions

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,14 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(item, array) {
+  
+  if(array.indexOf(item) === -1) {
+    array.push(item);
+  }
+  return array;
+};
+  
+  
 
-module.exports.reverseSortedList = undefined;
+
+module.exports.reverseSortedList = function(array) {
+  return array.sort().reverse();
+}
