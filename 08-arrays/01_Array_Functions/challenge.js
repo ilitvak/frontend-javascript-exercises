1e8628e2d3612f2514a9ebb32f48c8b79b035aa6
Irwin Litvak <irwin.litvak@gmail.com>
2014-10-09 20:51:35 +0000

    complete

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,22 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(array) {  
+  
+  var reversing = array.reverse();
+  array.unshift(1);
+  return reversing;
 
-module.exports.plusesEverywhere = undefined;
 
-module.exports.arrayQuantityPlusOne = undefined;
+}
+
+module.exports.plusesEverywhere = function(array) {
+
+    var adding = array.join("+");
+    return adding;
+  
+}
+
+module.exports.arrayQuantityPlusOne = function(array) {
+  
+  var quantity = array.length + 1;
+  return quantity;
+
+}
