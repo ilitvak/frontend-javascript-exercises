e81a50bba8843f1c41f2481e167ce7ca05fd12a9
Irwin Litvak <irwin.litvak@gmail.com>
2014-10-10 18:57:52 +0000

    Objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,32 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(title, duration, students) {
+  var course = {
+    title: title,
+    duration: duration,
+    students: students
+  };
+  return course;
+};
+module.exports.addProperty = function(object, newProp, newValue){
+  if(! object.hasOwnProperty(newProp)) {
+    object[newProp] = newValue;
+  }
+  return object;
+};
 
-module.exports.addProperty = undefined;
+module.exports.formLetter = function(letter){
+  var form = "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely," + "\n" + letter.sender;
+  return form;
+};
 
-module.exports.formLetter = undefined;
+module.exports.canIGet = function(item, money) {
+  var canIBuy = {
+    "MacBook Air": 999,
+    "MacBook Pro": 1299,
+    "Mac Pro": 2499,
+    "Apple Sticker": 1
+  };
+  return money >= canIBuy[item]; 
+};
 
-module.exports.canIGet = undefined;
+
+
