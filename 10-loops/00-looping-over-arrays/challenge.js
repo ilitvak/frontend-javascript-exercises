7c77a51fd2d7f2ffd3020a3522bc00033d2a7b6b
Irwin Litvak <irwin.litvak@gmail.com>
2014-10-12 23:09:57 +0000

    Looping over Arrays

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,20 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array){
+  var sum = 0;
+  for(var count = 0; count < array.length; count++){
+    sum += array[count]; // Ask Nick to explain this problem. 
+  }
+  return sum;
+} 
 
-module.exports.splitAndLowerCaseString = undefined;
-
-module.exports.addIndex = undefined;
+module.exports.splitAndLowerCaseString = function(inputString){
+  
+  var lower = inputString.toLowerCase();
+  var split = lower.split(",");
+  return split;
+}
+module.exports.addIndex = function(array){
+  return array.map(function(element, index){
+    return index + " is " + element; // Go over with Nick 
+  })
+  
+}
