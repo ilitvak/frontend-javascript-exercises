62a925b982c2e9bd0ab8c6391422ca046cfbfd57
Irwin Litvak <irwin.litvak@gmail.com>
2014-10-13 00:01:24 +0000

    iterating-over-objects

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,22 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function(object){
+  var keys = [];
+  for(var property in object){
+    keys.push(property);
+  }
+  return keys;
+}
 
-module.exports.getValues = undefined;
-
-module.exports.objectToArray = undefined;
+module.exports.getValues = function(object){
+  var keys = [];
+  for(var value in object){
+    keys.push(object[value]);
+  }
+  return keys;
+} 
+  
+module.exports.objectToArray = function(obj){
+  var keys = Object.keys(obj);
+  return keys.map(function(key) {
+    return key + " is " + obj[key]; 
+  });
+}
