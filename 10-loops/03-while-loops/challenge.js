b920f2af100e12321dffb8d810475fab43d15c26
Irwin Litvak <irwin.litvak@gmail.com>
2014-10-13 00:46:52 +0000

    while-loops

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,14 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn, actionFn){
+  while(conditionalFn()) {
+    actionFn();
+  }
+}
 
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array){
+  var number = 0;
+  while(array.length > 0){
+      number+=array.pop();
+  }
+  return number;
+}
 
